97142da3b40f5f5b778846b5330335455a42e4c3
berdibaevaulzada-eng <berdibaeva.ulzada@icloud.com>
2026-09-22 03:06:03 +0500

    lab03: YAML config, profiles, typed properties, actuator

--- src/main/java/kz/iitu/springlab/SpringLab01Application.java
+++ src/main/java/kz/iitu/springlab/SpringLab01Application.java
@@ -2,12 +2,13 @@ package kz.iitu.springlab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SpringLab01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLab01Application.class, args);
 	}
-
 }
